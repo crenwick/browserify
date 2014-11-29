ad7097301b8258ab66e801542d4c16118e134c41
Charles Renwick <crenwick@crenwick-mac.local>
2014-11-29 12:14:30 -0800

    cleaned up for test

--- app/js/client.js
+++ app/js/client.js
@@ -4,17 +4,3 @@ var React = require('react');
 var $ = require('jquery');
 
 $('#app').append('<p>Hello from javascript!</p>');
-
-var CommentBox = React.createClass({
-  render: function() {
-    return (
-      <div className="commentBox">
-        Hello, world, I am a CommentBox.
-      </div>
-    );
-  }
-});
-React.render(
-  <CommentBox />,
-  document.getElementById('content')
-);
