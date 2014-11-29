89f77e9fb8296c0dbfb622f0e98368f4d8817346
Charles Renwick <crenwick@crenwick-mac.local>
2014-11-29 12:08:31 -0800

    Updated the bower to include react and jsx files

--- app/js/client.js
+++ app/js/client.js
@@ -1,5 +1,20 @@
 'use strict';
 
+var React = require('react');
 var $ = require('jquery');
 
 $('#app').append('<p>Hello from javascript!</p>');
+
+var CommentBox = React.createClass({
+  render: function() {
+    return (
+      <div className="commentBox">
+        Hello, world, I am a CommentBox.
+      </div>
+    );
+  }
+});
+React.render(
+  <CommentBox />,
+  document.getElementById('content')
+);
